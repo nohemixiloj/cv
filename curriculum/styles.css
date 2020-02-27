8641ad59d2cd3d4924f3217bfe552609038bce82
nohemixiloj <61220208+nohemixiloj@users.noreply.github.com>
2020-02-27 12:08:11 -0600

    Add files via upload

---
+++ curriculum/styles.css
@@ -0,0 +1,40 @@
+*{
+	margin: 3;
+	padding: 3;
+	box-sizing: border-box;
+	background-color:#F38AD0
+}
+.body{
+	background-color:#F684CC;
+	padding: 12px;
+	width: 80%;
+	margin:0 auto;
+
+}
+.titulo2{
+	background-color:#F67BC9;
+	padding: 5px;
+	border-radius: 20px;
+	font-weight: bold;
+
+}
+.datos{
+	background-color:#EA73BF; 
+	padding: 15px;
+	border-radius: 50px;
+	font-weight: bold;
+}
+.estudios{
+background-color:#EA73BF; 
+	padding: 15px;
+	border-radius: 50px;
+	font-weight: bold;
+}
+.referencia{
+	background-color:#EA73BF; 
+	padding: 5px;
+	border-radius: 20px;
+	font-weight: bold;
+}
+
+}
